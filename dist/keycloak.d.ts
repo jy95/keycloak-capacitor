ee46cbcabe0398dfe93d1bbc97edaa422f6eb797
jy95 <jy95@users.noreply.github.com>
2022-08-14 17:17:58 +0000

    [auto-updater] update Keycloak to 19.0.1

--- dist/keycloak.d.ts
+++ dist/keycloak.d.ts
@@ -92,7 +92,7 @@ export interface KeycloakInitOptions {
 	 * });
 	 * ```
 	 */
-	adapter?: 'default' | 'cordova' | 'cordova-native' | 'capacitor' | 'capacitor-native' | KeycloakAdapter;
+	adapter?: 'default' | 'cordova' | 'cordova-native' | KeycloakAdapter;
 	
 	/**
 	 * Specifies an action to do on load.
