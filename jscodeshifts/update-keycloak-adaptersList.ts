cf39697d9a7c9f1f3ab7d6051e161378eeb4d4ab
Jacques Yakoub <jacques.yakoub@gmail.com>
2022-09-24 09:37:05 +0200

    Update update-keycloak-adaptersList.ts (#8)

--- jscodeshifts/update-keycloak-adaptersList.ts
+++ jscodeshifts/update-keycloak-adaptersList.ts
@@ -1,5 +1,7 @@
 import type { API, FileInfo } from 'jscodeshift';
 
+export const parser = "ts";
+
 export default function transformer(file: FileInfo, api: API) {
   const j = api.jscodeshift;
   const root = j(file.source);
